6c1d7c0927ceabbe65206bc67e6062eb0267753d
jessmama <jessmama14@gmail.com>
2017-10-15 21:19:49 +0800

    finish all program.

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,5 +1,16 @@
-import java.util.Scanner;
 
 public class GuessNumber {
+    private int numberIGuess;
 
+    String GuessNumber(int g,int s){
+        numberIGuess=g;
+        if (numberIGuess == s) {
+            return "Right";
+        }else if(numberIGuess>s){
+            return "It's smaller than you guess. ";
+        }else{
+            return "It's geater than you guess. ";
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args){
+
         GenerateNumber computerNumber=new GenerateNumber();
-        System.out.println(computerNumber.GenerateNumber());
+        int getComputerNumber=computerNumber.GenerateNumber();
+        System.out.println(getComputerNumber);
+
+        GuessNumber guess=new GuessNumber();
+
+        for (int leftTimes=9;leftTimes>=0;leftTimes--){
+
+            System.out.println("Guess a number between 1 to 100:");
+            Scanner scanner = new Scanner(System.in);
+            int guessNumber = scanner.nextInt();
+
+            String isRight=guess.GuessNumber(guessNumber,getComputerNumber);
+            if(isRight=="Right"){
+                System.out.println("You win!");
+                break;
+            }else{
+                if(leftTimes==0){
+                    System.out.println("Game Over.");
+                }else {
+                    System.out.println(isRight + " You left " + leftTimes + " times to guess.");
+                }
+            }
+
+        }
     }
 }
